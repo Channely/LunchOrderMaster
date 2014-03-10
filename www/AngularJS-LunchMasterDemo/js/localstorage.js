ceb13565af97864d183d0219ef3e846502fed6ea
Channely <qijie29896@gmail.com>
2014-03-10 13:54:24 +0800

    create the lunch-master-folder for using in the codedu server

---
+++ www/AngularJS-LunchMasterDemo/js/localstorage.js
@@ -0,0 +1,54 @@
+function store_the_order_information() {
+
+    var order_information = {
+        user: localStorage.getItem('user'),
+        restaurant: localStorage.getItem('restaurant'),
+        food: JSON.parse(localStorage.getItem('food_information_object')),
+        price_status: 'unknown'
+    }
+
+    order_information = setting_price_status(order_information)
+
+    var orders_list = JSON.parse(localStorage.getItem('orders_list'));
+    orders_list.push(order_information);
+    localStorage.setItem("orders_list", JSON.stringify(orders_list));
+
+}
+
+function setting_price_status(order_information_object) {
+    order_information_object.price_status = order_information_object.food.price > 12 ? 'beyond_price' : 'within_price';
+    return order_information_object
+}
+
+function clear_order_index_page_data() {
+    localStorage.setItem('user', '');
+    localStorage.setItem('food_information_object', '{"name":"","price":""}');
+}
+
+function get_the_unorders_people_name_array(unorders) {
+    var orders = JSON.parse(localStorage.getItem('orders_list'));
+    for (var i = 0; i < orders.length; i++) {
+        for (var j = 0; j < unorders.length; j++) {
+            orders[i].user == unorders[j].name ? unorders.splice(j, 1) : i = i;
+        }
+    }
+    return unorders
+}
+
+function get_total_price() {
+    var total_price = 0,
+        orders = JSON.parse(localStorage.getItem('orders_list'));
+
+    for (var i = 0; i < orders.length; i++) {
+        total_price += orders[i].food.price;
+    }
+
+    return parseFloat(total_price).toFixed(2)
+}
+
+function change_foods_price_to_float_format(orders) {
+    for (index in orders) {
+        orders[index].food.price = parseFloat(orders[index].food.price).toFixed(2);
+    }
+    return orders
+}
---
+++ www/AngularJS-LunchMasterDemo/js/routes.js
@@ -0,0 +1,47 @@
+myModule.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "pages/home_page.html",
+        controller: HomePageController
+    }).when("/home_page", {
+            templateUrl: "pages/home_page.html",
+            controller: HomePageController
+        }).when("/help_ordering_page", {
+            templateUrl: "pages/help_ordering_page.html",
+            controller: HelpOrderingController
+        }).when("/choose_people_page", {
+            templateUrl: "pages/choose_people_page.html",
+            controller: ChoosePeopleController
+        }).when("/choose_restaurant_page", {
+            templateUrl: "pages/choose_restaurant_page.html",
+            controller: ChooseRestaurantController
+        }).when("/choose_package_page", {
+            templateUrl: "pages/choose_package_page.html",
+            controller: ChoosePackageController
+        }).when("/orders_show_page", {
+            templateUrl: "pages/orders_show_page.html",
+            controller: OrdersShowController
+        }).otherwise({
+            redirectTo: "/"
+        });
+
+
+    //routing generate
+    //routing generated over
+});
+
+/** Here is example
+ myModule.config(function($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "pages/activity_list_page.html",
+        controller: ActivityListController
+    }).when("/activity/create", {
+            templateUrl: "pages/activity_create_page.html",
+            controller: ActivityCreateController
+        }).when("/sign_ups/list/:activity_name", {
+            templateUrl: "pages/apply_page.html",
+            controller: SignUpListController
+        }).otherwise({
+            redirectTo: "/"
+        });
+});
+ **/
